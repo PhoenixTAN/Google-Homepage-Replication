6a373557f5ad093d872732e7c03455b1949711b0
PhoenixTAN <tanziqi1756@outlook.com>
2020-05-27 00:41:44 -0400

    Use stopPropagation to make floating popup window dispear.

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 
-function showGoogleApps() {
+var googleAppsIcon = document.getElementsByClassName("head-right-google-apps")[0];
+var setting = document.getElementById("setting");
+
+googleAppsIcon.onclick = function(event) {
     var googleApps = document.getElementById("floating-popup-apps");
 
     if ( googleApps.style.display == "block" ) {
         googleApps.style.display = "none";
-        console.log("return");
         return ;
     }
 
     googleApps.style.display = "block";
+    event.stopPropagation();
+    /* 
+        prevents further propagation of the current event 
+        in the capturing and bubbling phases.
+    */
+}
 
-    /* 全局鼠标点击的监听器 */ 
-    /*
-    var clientX = window.event.clientX;
-    var clientY = window.event.clientY;
+setting.onclick = function(event) {
+    var settingPopup = document.getElementById("setting-floating-popup");
+    if ( settingPopup.style.display == "block" ) {
+        settingPopup.style.display = "none";
+        return ;
+    }
+    settingPopup.style.display = "block";
+    event.stopPropagation();
+}
+
+window.onclick = function(event) {
+
+    var clientX = event.clientX;
+    var clientY = event.clientY;
+
+    /* make the google apps popup window disappear */
+    var googleApps = document.getElementById("floating-popup-apps");
     var position = googleApps.getBoundingClientRect();
+
     if ( clientX < position.left || clientX > position.right || 
         clientY > position.bottom || clientY < position.top ) {
-        console.log("outside");
         googleApps.style.display = "none"
     }
-    else {
-        console.log("inside");
-    }
-    */
 
+    /* make the setting floating popup window disppear */
+    var settingPopup = document.getElementById("setting-floating-popup");
+    position = settingPopup.getBoundingClientRect();
+    if ( clientX < position.left || clientX > position.right || 
+        clientY > position.bottom || clientY < position.top ) {
+        settingPopup.style.display = "none"
+    }
 }
+
+
 
